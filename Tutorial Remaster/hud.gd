402a4bdff86afe030a3012a5530084a34ebbea45
Culn <euckmog001@eucnordvest.dk>
2025-02-17 09:07:13 +0100

    Web Commit 2

--- Tutorial Remaster/hud.gd
+++ Tutorial Remaster/hud.gd
@@ -12,7 +12,7 @@ func show_game_over():
 	
 	await $MessageTimer.timeout
 	
-	$Message.text = "Dodge the Creeps"
+	$Message.text = "Dodge the creeps!"
 	$Message.show()
 	
 	await get_tree().create_timer(1.0).timeout
